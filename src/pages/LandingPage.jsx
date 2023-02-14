2f881e570ad5473ff3e762f7130f05b15a663b96
Goose9x <dangtandung99@gmail.com>
2023-02-14 10:44:56 +0700

    add Benefit design

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,29 @@
 import "./LandingPage.scss";
 import bigImage from "../assets/big-image.png";
+import Card from "react-bootstrap/Card";
+import Benefit1 from "../assets/Benefit1.png";
+import Benefit2 from "../assets/Benefit2.png";
+import Benefit3 from "../assets/Benefit3.png";
+const benefitData = [
+  {
+    logoPreview: Benefit1,
+    title: "Many Choices",
+    content:
+      "Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris non.",
+  },
+  {
+    logoPreview: Benefit2,
+    title: "Fast and On Time",
+    content:
+      "Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris non.",
+  },
+  {
+    logoPreview: Benefit3,
+    title: "Affordable Price",
+    content:
+      "Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris non.",
+  },
+];
 function LandingPage() {
   return (
     <>
@@ -28,6 +52,35 @@ function LandingPage() {
           Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim
           placerat nisi, adipiscing mauris non purus parturient.
         </p>
+      </div>
+      <div className='lp-benefit-cards'>
+        {/* Có thể đưa 1 BenefitCard component vào đây */}
+        {benefitData.map((e) => (
+          <Card className='lp-benefit-item'>
+            <Card.Img variant='top' src={e.logoPreview} />
+            <Card.Body>
+              <Card.Title>
+                <b>{e.title}</b>
+              </Card.Title>
+              <Card.Text>
+                <p>{e.content}</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+      <div className='lp-popular-product'>
+        <div className='lp-popular-product-describle'>
+          <span>Product</span>
+          <b>Our popular product</b>
+          <p>
+            Pellentesque etiam blandit in tincidunt at donec. Eget ipsum
+            dignissim placerat nisi, adipiscing mauris non purus parturient.
+          </p>
+        </div>
+        <div className="lp-popular-product-item">
+            
+        </div>
       </div>
     </>
   );
